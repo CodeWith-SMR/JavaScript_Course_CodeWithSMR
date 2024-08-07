79bca4961765ca0e28f190e75e61cdf0b7b82e2b
Muhammad Raza <codewithsmr@gmail.com>
2024-08-07 20:46:35 +0500

    functions concept

---
+++ Class 12 Date and time/app.js
@@ -0,0 +1,187 @@
+// let now = new Date();
+
+// console.log(now);
+
+// let dayName = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+// let day = now.getDay();
+// // console.log(day);
+// console.log(dayName[day]);
+
+
+
+
+
+
+// let monthName = ["Jan", "Feb", "Mar", "Apr"];
+
+// let month = now.getMonth();
+// console.log(month);
+// console.log(monthName[month]);
+
+
+
+
+
+
+
+
+
+
+
+
+// let ms = now.getMilliseconds();
+// console.log(ms);
+
+// let msNow = now.getTime();
+// console.log(msNow);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let dob = new Date("October 4, 1991");
+// let now = new Date();
+
+// let age = now.getTime() - dob.getTime()
+
+// console.log(age / 1000 / 60 / 60 / 24 / 365.25);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function addDashOnOddNum(str) {
+//     if (!isNaN(str)) {
+//         let newStr = "";
+
+//         for (let i = 0; i < str.length; i++) {
+//             if (
+//                 str[i] % 2 !== 0 &&
+//                 str[i + 1] % 2 !== 0 &&
+//                 !isNaN(str[i + 1] % 2)
+//             ) {
+//                 newStr += str[i] + "-";
+//             }
+//             else {
+//                 newStr += str[i];
+//             }
+//         }
+//         console.log(newStr);
+
+//     }
+//     else {
+//         console.log('Number required');
+//     }
+// }
+
+// addDashOnOddNum("23497412579");
+// addDashOnOddNum("5796213");
+// addDashOnOddNum("588a5288");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let today = new Date();
+// let startDate = new Date("December 11, 2023");
+
+// let diff = today - startDate;
+
+// console.log(diff / 1000 / 60 / 60 / 24 / 30);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let dob = new Date("October 20, 2024");
+// console.log(dob);
+
+// dob.setDate(4);
+
+// console.log(dob);
+
+// dob.setFullYear(2025);
+
+// console.log(dob);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
